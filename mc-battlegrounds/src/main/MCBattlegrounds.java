40a5666624890b496cccdf1553806392cabde759
BuildTools <unconfigured@null.spigotmc.org>
2017-12-28 16:05:16 +0100

    Initialize plugin project

---
+++ mc-battlegrounds/src/main/MCBattlegrounds.java
@@ -0,0 +1,24 @@
+package main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class MCBattlegrounds extends JavaPlugin {
+
+	@Override
+	public void onLoad() {
+	
+	}
+	
+	
+	@Override
+	public void onEnable() {
+		System.out.println("Plugin MC-Battlegrounds loaded.");
+	}
+	
+	
+	@Override
+	public void onDisable() {
+		System.out.println("Disabling MC_Battlegrounds");
+	}
+}
